aac01d904b8d314e2c1d2a6a01e639f9e4ee1247
HimaniPant <hp.pant0702@gmail.com>
2022-02-14 20:54:11 +0530

    Initial code

---
+++ JenkinsCI-CD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JenkinsCI_CD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
